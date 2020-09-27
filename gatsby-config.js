e240f12a02e957058bb51aca304e8d05746eb49d
Nicholas Duffy <duffn@outlook.com>
2020-09-27 07:34:34 -0600

    Remove offline and manifest plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,31 +48,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `nicholasduffy.com`,
-        short_name: `nicholasduffy.com`,
-        description: `I write about Node.js, Go, Python, infrastructure, and data.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#6B46C1`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
